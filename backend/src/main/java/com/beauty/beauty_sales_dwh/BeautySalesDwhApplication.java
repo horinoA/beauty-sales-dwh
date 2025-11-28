9578de8e8eac6ead08f1daedffdb744d213cf5fd
horinoA <horinoakiko@gmail.com>
2025-11-28 09:25:11 +0900

    springbootPJ追加

---
+++ backend/src/main/java/com/beauty/beauty_sales_dwh/BeautySalesDwhApplication.java
@@ -0,0 +1,13 @@
+package com.beauty.beauty_sales_dwh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautySalesDwhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeautySalesDwhApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/beauty/beauty_sales_dwh/BeautySalesDwhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beauty.beauty_sales_dwh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautySalesDwhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
