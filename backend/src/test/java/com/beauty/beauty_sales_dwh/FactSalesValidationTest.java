dc20f8ed03170ba2b9ce46c5a282789fa3440970
horinoA <horinoakiko@gmail.com>
2025-12-10 11:19:27 +0900

    FactSalesテスト完了

--- backend/src/test/java/com/beauty/beauty_sales_dwh/FactSalesValidationTest.java
+++ backend/src/test/java/com/beauty/beauty_sales_dwh/FactSalesValidationTest.java
@@ -2,6 +2,8 @@ package com.beauty.beauty_sales_dwh;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
 import java.util.Set;
 
 import org.junit.jupiter.api.Test;
@@ -24,8 +26,8 @@ public class FactSalesValidationTest {
 
         FactSales invalidSales = new FactSales(
             null, // CompanyID null -> エラー
-            "TX123",
-            null, null, null, null, null, 
+            "1",
+            OffsetDateTime.now(), LocalDate.now(), "1", "1", "1", 
             1000, 1000, 1100, 1000, 0, 0, 0, 0, 0, 
             "SALES", 
             false
@@ -49,8 +51,8 @@ public class FactSalesValidationTest {
         
        FactSales invalidSales = new FactSales(
             0L,// CompanyID 0 -> Snowflakeエラー
-            "110",
-            null, null, null, null, null, 
+            "1",
+            OffsetDateTime.now(), LocalDate.now(), "1", "1", "1", 
             1000, 1000, 1100, 1000, 0, 0, 0, 0, 0, 
             "SALES", 
             false
@@ -71,13 +73,116 @@ public class FactSalesValidationTest {
     }
 
     @Test
+    void smaregiIdOKtest(){
+        //スマレジIDが範囲内ならOK
+        FactSales invalidSales = new FactSales(
+            4069L,
+            "2", 
+            OffsetDateTime.now(), LocalDate.now(), "1", "1", "1", 
+            1000, 1000, 1100, 1000, 0, 0, 0, 0, 0, 
+            "SALES", 
+            false
+        );
+
+        // Act
+        Set<ConstraintViolation<FactSales>> violations = validator.validate(invalidSales);
+
+        // Assert
+        assertThat(violations).isEmpty();
+
+    }
+
+    @Test
+    void smaregiIdrenge(){
+        FactSales invalidSales = new FactSales(
+            4069L,
+            "0", //スマレジIDが1以下ならエラー
+            OffsetDateTime.now(), LocalDate.now(), "1", "1", "1", 
+            1000, 1000, 1100, 1000, 0, 0, 0, 0, 0, 
+            "SALES", 
+            false
+        );
+
+        // Act
+        Set<ConstraintViolation<FactSales>> violations = validator.validate(invalidSales);
+
+        // Assert
+        assertThat(violations).isNotEmpty();
+
+        violations.forEach(constraintViolation -> {
+            String msg =
+                "message = " + constraintViolation.getMessage() + "\n" +
+                "messageTemplate = " + constraintViolation.getMessageTemplate() + "\n" +
+                "rootBean = " + constraintViolation.getRootBean() + "\n" +
+                "rootBeanClass = " + constraintViolation.getRootBeanClass() + "\n" +
+                "invalidValue = " + constraintViolation.getInvalidValue() + "\n" +
+                "propertyPath = " + constraintViolation.getPropertyPath() + "\n" +
+                "leafBean = " + constraintViolation.getLeafBean() + "\n" +
+                "descriptor = " + constraintViolation.getConstraintDescriptor() + "\n"
+            ;
+            
+            System.out.println(msg);
+        });
+
+        // メッセージがプロパティファイルから取得できているか確認
+        boolean messageFound = violations.stream()
+            .anyMatch(v -> v.getMessage().equals("IDは1〜999999999の間で指定してください"));
+            
+        assertThat(messageFound).isTrue();
+
+
+        FactSales invalidSales_1 = new FactSales(
+            4069L,
+            "9999999999", //スマレジIDが999999999以上ならエラー
+            OffsetDateTime.now(), LocalDate.now(), "1", "1", "1", 
+            1000, 1000, 1100, 1000, 0, 0, 0, 0, 0, 
+            "SALES", 
+            false
+        );
+
+        // Act
+        Set<ConstraintViolation<FactSales>> violations_1 = validator.validate(invalidSales_1);
+
+        // Assert
+        assertThat(violations_1).isNotEmpty();
+
+        // メッセージがプロパティファイルから取得できているか確認
+        boolean messageFound_1 = violations_1.stream()
+            .anyMatch(v -> v.getMessage().equals("IDは1〜999999999の間で指定してください"));
+            
+        assertThat(messageFound_1).isTrue();
+
+    }
+
+    @Test
+    void sumareziNullOk(){
+        FactSales invalidSales = new FactSales(
+            4069L,
+            "1",
+            OffsetDateTime.now(), LocalDate.now(), 
+            "", null, "1", //@ValidSmaregiIdは空白やnullは許可
+            1000, 1000, 1100, 1000, 0, 0, 0, 0, 0, 
+            "SALES", 
+            false
+        );
+
+        // Act
+        Set<ConstraintViolation<FactSales>> violations = validator.validate(invalidSales);
+
+        // Assert
+        assertThat(violations).isEmpty();
+
+    }
+
+
+    @Test
     void validateTranType(){
        FactSales invalidSales = new FactSales(
-            4096L,
-            "110",
-            null, null, null, null, null, 
+            4069L,
+            "2", 
+            OffsetDateTime.now(), LocalDate.now(), "1", "1", "1", 
             1000, 1000, 1100, 1000, 0, 0, 0, 0, 0, 
-            "", //tranTypeが指定値以外
+            "",//tranTypeが指定値以外 
             false
         );
 
@@ -93,12 +198,12 @@ public class FactSalesValidationTest {
             
         assertThat(messageFound).isTrue();
 
-       FactSales invalidSales_1 = new FactSales(
-            4096L,
-            "110",
-            null, null, null, null, null, 
+        FactSales invalidSales_1 = new FactSales(
+            4069L,
+            "2", 
+            OffsetDateTime.now(), LocalDate.now(), "1", "1", "1", 
             1000, 1000, 1100, 1000, 0, 0, 0, 0, 0, 
-            null, //tranTypeがNull
+            null,//tranTypeがnull 
             false
         );
 
@@ -118,15 +223,19 @@ public class FactSalesValidationTest {
     @Test
     void validTransactionAmount(){
 
-       FactSales invalidSales = new FactSales(
-            4096L,
-            "110",
-            null, null, null, null, null, 
-            -1000, 
-            1000, 1100, 1000, 0, 0, 0, 0, 0, 
-            "SALES", //totalがー（返金処理）だがtranTypeが入金
+        FactSales invalidSales = new FactSales(
+            4069L,
+            "2", 
+            OffsetDateTime.now(), LocalDate.now(), "1", "1", "1", 
+            -1000, //totalがー（返金処理）だがtranTypeが入金
+            1000, 
+            1100, 
+            1000, 
+            0, 0, 0, 0, 0, 
+            "SALES", 
             false
         );
+        
 
         // Act
         Set<ConstraintViolation<FactSales>> violations = validator.validate(invalidSales);
@@ -141,12 +250,15 @@ public class FactSalesValidationTest {
         assertThat(messageFound).isTrue();
 
         FactSales invalidSales_1 = new FactSales(
-            4096L,
-            "110",
-            null, null, null, null, null, 
+            4069L,
+            "2", 
+            OffsetDateTime.now(), LocalDate.now(), "1", "1", "1", 
+            1000, //totalが+（入金処理）だがtranTypeが返金
             1000, 
-            1000, 1100, 1000, 0, 0, 0, 0, 0, 
-            "REFUND", //totalが+（入金処理）だがtranTypeが返金
+            1100, 
+            1000, 
+            0, 0, 0, 0, 0, 
+            "REFUND", 
             false
         );
 
@@ -163,5 +275,6 @@ public class FactSalesValidationTest {
         assertThat(messageFound_1).isTrue();
 
     }
+
 
 }
