84b7454662ea32341e74d0932ed5fba915a1c2da
horinoA <horinoakiko@gmail.com>
2026-02-18 13:51:45 +0900

    TransactionRawDataコメント修正

--- backend/src/main/java/com/beauty/beauty_sales_dwh/domain/TransactionRawData.java
+++ backend/src/main/java/com/beauty/beauty_sales_dwh/domain/TransactionRawData.java
@@ -8,7 +8,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class TransactionRawData {
-    private Long transactionId; //挿入ずみのTransavtion_id1
+    private Long transactionId; //挿入ずみのTransavtion_id
     private String jsonBody;   // APIレスポンス(JSON文字列)
     private Long companyId; // 企業ID
 }
